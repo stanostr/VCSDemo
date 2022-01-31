fc950d634e2ee720cbe3aec2d9290b67d6a177fc
stanostr <stan_ostr@yahoo.com>
2022-01-31 00:44:56 -0500

    initial commit

---
+++ src/com/demo/util/ArithmeticUtil.java
@@ -0,0 +1,39 @@
+package com.demo.util;
+
+public class ArithmeticUtil {
+	private ArithmeticUtil() {
+	}
+
+	/**
+	 * Returns the sum of two integers
+	 * 
+	 * @param a first integer
+	 * @param b second integer
+	 * @return sum
+	 */
+	public static Integer sum(Integer a, Integer b) {
+		return a + b;
+	}
+
+	/**
+	 * Returns the difference of two integers
+	 * 
+	 * @param a the minuend
+	 * @param b the subtrahend
+	 * @return difference between a and b
+	 */
+	public static Integer difference(Integer a, Integer b) {
+		return a - b;
+	}
+
+	/**
+	 * Returns the product of two integers
+	 * 
+	 * @param a first integer
+	 * @param b second integer
+	 * @return product
+	 */
+	public static Integer product(Integer a, Integer b) {
+		return a * b;
+	}
+}
